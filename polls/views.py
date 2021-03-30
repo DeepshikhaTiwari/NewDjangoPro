41564be59c98e04be259d316cad5217fd0240193
Deepshikha Tiwari <shikhablb1@gmail.com>
2021-03-30 13:12:14 +0530

    Add 'Index()'
    Which displays the latest 5 poll questions in the system, separated by commas, according to publication date.

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
-
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello ths is my first view, and you are at the poll index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 
 def detail(request, question_id):
