7bc6e5f976de315a6730a2d67aa6861da01246a5
Deepshikha Tiwari <66195580+DeepshikhaTiwari@users.noreply.github.com>
2021-03-30 12:02:22 +0530

    Add 'Detail', 'Result', 'Vote'

--- polls/views.py
+++ polls/views.py
@@ -3,3 +3,16 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello ths is my first view, and you are at the poll index.")
+
+
+def detail(request, question_id):
+    return HttpResponse("You are looking at question %s." %question_id)
+
+
+def results(request, question_id):
+    response = "You are looking at result of quesion %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You are voting on question %s." %question_id)
